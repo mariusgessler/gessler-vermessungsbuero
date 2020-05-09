f29b8507d05af3d4b1dce2c51bd30c791f9563d8
mariusgessler <marius.gessler@gmail.com>
2020-05-09 14:29:52 +0200

    set up contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,45 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+}
+
+// if you want to use the preview API please define
+// CONTENTFUL_HOST in your environment config
+// the `host` property should map to `preview.contentful.com`
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the access token need to be provided.'
+  )
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gessler Vermessungsbüro`,
     description: `Ingenieurbüro für Vermessung Helmut Gessler`,
     author: `Marius Gessler`,
   },
+  pathPrefix: '/gessler-vermessungsbuero',
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    `@contentful/gatsby-transformer-contentful-richtext`,
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -24,11 +49,8 @@ module.exports = {
         background_color: `#1470AF`,
         theme_color: `#1470AF`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `static/favicon.png`, // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
