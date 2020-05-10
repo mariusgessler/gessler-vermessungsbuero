82d1f6bd89cf1cfb7a64d27186372a998b64f2f5
mariusgessler <marius.gessler@gmail.com>
2020-05-10 19:44:47 +0200

    updated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,12 +4,16 @@ import Address from './address';
 
 const Footer = () => (
   <div className="section section--footer">
-    <div className="row">
-      <div className="col col-xs-12 col-sm-6">
-        <Address />
-      </div>
-      <div className="col col-xs-12 col-md-6">
-        <Link to="/impressum/">Datenschutz und Impressum</Link>
+    <div className="container">
+      <div className="container-fluid">
+        <div className="row between-sm">
+          <div className="col col-xs-12 col-sm-6">
+            <Address />
+          </div>
+          <div className="col col-xs-12 col-sm-6 end-sm link">
+            <Link className="link--impressum" to="/impressum/">Datenschutz und Impressum</Link>
+          </div>
+        </div>
       </div>
     </div>
   </div>
