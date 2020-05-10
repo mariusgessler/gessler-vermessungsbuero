3fd574956ca38908eb529d9f4b2f1e30e35723fd
mariusgessler <marius.gessler@gmail.com>
2020-05-10 19:45:04 +0200

    updated index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,14 +4,20 @@ import Hero from '../components/hero';
 import Intro from '../components/intro';
 import Leistungen from '../components/leistungen';
 import Footer from '../components/footer';
+import SEO from '../components/nanos/SEO';
 import '../css/style.scss';
 
 const Index = () => (
   <>
+    <SEO />
     <Topbar />
-    <Hero />
-    <Intro />
-    <Leistungen />
+    <div className="container">
+      <div className="container-fluid">
+        <Hero />
+        <Intro />
+        <Leistungen />
+      </div>
+    </div>
     <Footer />
   </>
 );
